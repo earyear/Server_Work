cb2b2516e2db46732dab9d323db3e479e0a81f83
NamGwiYeon <earkite.n@gmail.com>
2024-05-13 16:13:50 +0900

    mini_practice 서버연결

---
+++ mini_practice_0513/src/main/java/com/busanit501/mini_practice_0513/MyServlet3.java
@@ -0,0 +1,30 @@
+package com.busanit501.mini_practice_0513;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "myServlet3", value = "/servlet3")
+public class MyServlet3 extends HttpServlet {
+    private String message;
+
+    public void init() {
+        message = "Hello World! hahaha";
+    }
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
+
+        // Hello
+        PrintWriter out = response.getWriter();
+        out.println("<html><body>");
+        out.println("<h1>" + message + "</h1>");
+        out.println("</body></html>");
+    }
+
+    public void destroy() {
+    }
+}
